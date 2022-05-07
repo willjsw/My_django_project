08bfef7f8eb84878311bbd70575f6487c6507b73
willjsw <104188728+willjsw@users.noreply.github.com>
2022-05-07 20:52:19 +0900

    Add files via upload

--- Myapp/views.py
+++ Myapp/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+topics = [
+    {"id":1, "title":"routing", "body":"routing is.."},
+    {"id":2, "title":"view", "body":"view is.."},
+    {"id":3, "title":"Model", "body":"Model is.."},
+]
+
+nextId = 4
+
+def HTMLTemplate(article):
+    global topics
+    ol = ""
+    for topic in topics:
+        ol +=f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return f'''
+    <html>
+    <body>
+        <h1><a href="/">Django</a></h1>
+        <ol>
+            {ol}
+        </ol>
+        {article}
+        <li>
+            <a href="create">Create</a>
+        </li>
+    </body>
+    </html>
+    '''
+
+def index(request):
+    article = '''
+    <h2>Welcome</h2>
+    Hello Django
+    '''
+    return HttpResponse(HTMLTemplate(article))
+
+
+def read(request, id):
+    global topics
+    article = ""
+    for topic in topics:
+        if topic["id"] == int(id):
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    return HttpResponse(HTMLTemplate(article))
+
+@csrf_exempt
+def create(request):
+    global nextId
+    if request.method == "GET":
+        article = '''
+            <form action="/create/" method="POST">
+                <p><input type="text" placeholder="title" name="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(HTMLTemplate(article))
+    else:
+        title = request.POST['title']
+        body = request.POST['body']
+        newTopic = {"id":nextId, "title":title, "body":body}
+        topics.append(newTopic)
+        url = '/read/'+str(nextId)
+        nextId +=1
+        return redirect(url)
